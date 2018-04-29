e96d089310f02c95a4b5974171917d9de6c1fd33
Doga Oztuzun <unquale@gmail.com>
2018-04-29 17:33:41 +0300

    Bootstrap 4 eklendi ve layout degistirildi

--- src/Tdt.Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ src/Tdt.Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,18 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.css']
 })
-export class NavMenuComponent {
-  isExpanded = false;
+export class NavMenuComponent implements OnInit {
+  constructor() { }
 
-  collapse() {
-    this.isExpanded = false;
-  }
-
-  toggle() {
-    this.isExpanded = !this.isExpanded;
+  ngOnInit() {
   }
 }
